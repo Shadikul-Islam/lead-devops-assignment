fc18c93b48fa6d63d8e087a3bf9cbf5cb165a506
Md. Shadikul Islam <shadikul.islam.shuvo@gmail.com>
2026-05-09 14:34:44 +0000

    Added GitHub Actions CI/CD with AWS OIDC authentication and EKS deployment

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "github_actions" {
           }
 
           StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/master"
           }
         }
       }
